69e08cafe76e729fb6b1717749cb049344c5ead1
BuccoBruce <dcmcmillan@gmail.com>
2023-08-21 14:36:32 -0700

    Day one of 100 day challenge!

---
+++ 1/main.py
@@ -0,0 +1,7 @@
+# Day one of Python 100 day coding challenge
+
+print("Welcome to Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+petName = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + petName)
