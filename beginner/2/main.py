414648e8381ec6da51c346018571c386cef16080
BuccoBruce <dcmcmillan@gmail.com>
2023-08-22 15:33:03 -0700

    Cleaned up code for day 2 with f strings

--- beginner/2/main.py
+++ beginner/2/main.py
@@ -1,10 +1,12 @@
 print("Welcome to the tip calculator.")
+
 totalBill = float(input("What was the total bill? $"))
 peopleSplit = int(input("How many people to split the bill? "))
-percentTip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-totalToPay = 0.0
+# Add one to percent to get correct multiplier
+percentTip = 1 + float(input("What percentage tip would you like to give? 10, 12, or 15? ")) / 100
 
-totalToPay = float((totalBill * (1 + (percentTip / 100))) / peopleSplit)
+# Round total each person must pay to 1 significant digit
+totalToPay = round((totalBill * percentTip) / peopleSplit, 2)
 
 # Reduce significant digits of totalToPay to 1 in output
-print("Each person should pay: ${:.1f}".format(totalToPay))
+print(f"Each person should pay: ${totalToPay}")
