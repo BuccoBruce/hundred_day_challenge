fa8d528751a09608e16f801a0b4db88bdd4c8a5c
buccobruce <dcmcmillan@gmail.com>
2023-10-30 17:45:38 -0700

    added data save functionality to add button

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -3,11 +3,20 @@ from tkinter import *
 WHITE = "#ffffff"
 EMAIL_ADDRESS = "dcmcmillan@gmail.com"
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save_data():
-    pass
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("data.csv", "a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+    
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,32 +32,30 @@ canvas.grid(column=1, row=0)
 
 # Labels
 website_label = Label(text="Website:")
-email_username_label = Label(text="Email/Username:")
+email_label = Label(text="Email/Username:")
 password_label = Label(text="Password:")
 
 website_label.grid(column=0, row=1)
-email_username_label.grid(column=0, row=2)
+email_label.grid(column=0, row=2)
 password_label.grid(column=0, row=3)
 
 # Entry Fields
 website_entry = Entry(width=35)
-email_username_entry = Entry(width=35)
+email_entry = Entry(width=35)
 password_entry = Entry(width=21)
 
 website_entry.focus()
-email_username_entry.insert(0, EMAIL_ADDRESS)
+email_entry.insert(0, EMAIL_ADDRESS)
 
 website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
-email_username_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+email_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
 password_entry.grid(column=1, row=3, sticky="W")
 
 # Buttons
 gen_pass_button = Button(text="Generate Password")
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_data)
 
 gen_pass_button.grid(column=2, row=3, sticky="EW")
 add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
-
-
 
 window.mainloop()
