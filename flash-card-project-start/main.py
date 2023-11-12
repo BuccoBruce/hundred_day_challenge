d6003ce453b66b1f6d6fdcf82e0ef0b1c648fb0d
buccobruce <dcmcmillan@gmail.com>
2023-11-12 15:13:20 -0800

    Started flash card layout

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -1,2 +1,31 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR, highlightthickness=0)
+
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+
+# Flash Card
+canvas = Canvas(width=800, height=526)
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
+canvas.grid(column=0, row=0, columnspan=2)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+
+# Buttons
+cross_image = PhotoImage(file="images/wrong.png")
+check_image = PhotoImage(file="images/right.png")
+
+unknown_button = Button(image=cross_image, highlightthickness=0, border=0)
+known_button = Button(image=check_image, highlightthickness=0, border=0)
+
+unknown_button.grid(column=0, row=1)
+known_button.grid(column=1, row=1)
+
+window.mainloop()
